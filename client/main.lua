917da6c06698310925fe9c0efa0d69a60095119a
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-03-06 16:45:25 -0500

    tweak(client): run instance thread on globalInstances change Check whether the instance thread should run on every change in globalInstances from server

--- client/main.lua
+++ client/main.lua
@@ -2,10 +2,50 @@ local instances = GlobalState[Shared.State.globalInstances]
 local currentInstance = nil
 local playerId = PlayerId()
 local playerServerId = GetPlayerServerId(playerId)
+local isThreadRunning = false
 
-AddStateBagChangeHandler(Shared.State.globalInstances, nil, function(_, _, value)
-    instances = value
-end)
+local function runInstanceThread()
+    if isThreadRunning or not currentInstance then return end
+
+    CreateThread(function()
+        local localPlayerPed = PlayerPedId()
+        NetworkSetEntityInvisibleToNetwork(localPlayerPed, true) -- hide your ped from everyone
+
+        while isThreadRunning and currentInstance do
+            localPlayerPed = PlayerPedId()
+
+            SetLocalPlayerVisibleLocally(true)  -- show your ped to yourself
+
+            for instanceName, instancePlayers in pairs(instances) do
+                if instanceName ~= currentInstance then
+                    for i = 1, #instancePlayers do
+                        local player = GetPlayerFromServerId(instancePlayers[i])
+
+                        if player and NetworkIsPlayerActive(player) then
+                            local playerPed = GetPlayerPed(player)
+                            SetEntityNoCollisionEntity(playerPed, localPlayerPed, true)
+                        end
+                    end
+                else
+                    for i = 1, #instancePlayers do
+                        local player = GetPlayerFromServerId(instancePlayers[i])
+
+                        if player and NetworkIsPlayerActive(player) then
+                            local playerPed = GetPlayerPed(player)
+                            SetEntityLocallyVisible(playerPed)
+                            -- SetPlayerVisibleLocally(playerPed, true)
+                        end
+                    end
+                end
+            end
+
+            Wait(0)
+        end
+
+        NetworkSetEntityInvisibleToNetwork(localPlayerPed, false) -- show your ped to everyone
+        isThreadRunning = false
+    end)
+end
 
 local function enterInstance(instanceName)
     if not instanceName then return false, "instance_not_valid" end
@@ -18,75 +58,16 @@ local function enterInstance(instanceName)
 end
 exports("enterInstance", enterInstance)
 
-local function runInstanceThread()
-    if currentInstance then return end
-    CreateThread(function()
-        local localPlayerPed = PlayerPedId()
-        NetworkSetEntityInvisibleToNetwork(localPlayerPed, true) -- hide your ped for everyone
-        while currentInstance do
-            localPlayerPed = PlayerPedId()
-            SetLocalPlayerVisibleLocally(true)  -- show your ped for to yourself
-            for instanceName, instancePlayers in pairs(instances) do
-                if instanceName ~= currentInstance then
-                    for i = 1, #instancePlayers do
-                        local player = GetPlayerFromServerId(instancePlayers[i])
-                        if player and NetworkIsPlayerActive(player) then
-                            local playerPed = GetPlayerPed(player)
-                            SetEntityNoCollisionEntity(playerPed, localPlayerPed, true)
-                        end
-                    end
-                else
-                    for i = 1, #instancePlayers do
-                        local player = GetPlayerFromServerId(instancePlayers[i])
-                        if player and NetworkIsPlayerActive(player) then
-                            local playerPed = GetPlayerPed(player)
-                            -- SetEntityLocallyVisible(playerPed)
-                            SetPlayerVisibleLocally(playerPed, true)
-                        end
-                    end
-                end
-            end
-            Wait(0)
-        end
-        NetworkSetEntityInvisibleToNetwork(localPlayerPed, false) -- show your ped for everyone
-    end)
-end
---[[
-local function runInstanceThread()
-    if currentInstance then return end
-    CreateThread(function()
-        local localPlayerPed = PlayerPedId()
-        NetworkSetEntityInvisibleToNetwork(localPlayerPed, true) -- hide your ped for everyone
-        while currentInstance do
-            localPlayerPed = PlayerPedId()
-            SetLocalPlayerVisibleLocally(true)  -- show your ped for to yourself
-            for instanceName, instancePlayers in pairs(instances) do
-                for i = 1, #instancePlayers do
-                    local player = GetPlayerFromServerId(instancePlayers[i])
-                    if player and NetworkIsPlayerActive(player) then
-                        local playerPed = GetPlayerPed(player)
-                        if instanceName ~= currentInstance then
-                            SetEntityNoCollisionEntity(playerPed, localPlayerPed, true)
-                        else
-                            -- SetEntityLocallyVisible(playerPed)
-                            SetPlayerVisibleLocally(playerPed, true)
-                        end
-                    end
-                end
-            end
-            Wait(0)
-        end
-        NetworkSetEntityInvisibleToNetwork(localPlayerPed, false) -- show your ped for everyone
-    end)
-end
-]]
+AddStateBagChangeHandler(Shared.State.globalInstances, nil, function(_, _, value)
+    instances = value
+
+    runInstanceThread()
+end)
 
 AddStateBagChangeHandler(Shared.State.playerInstance, ("player:%s"):format(playerServerId), function(bagName, _, value)
     local source = GetPlayerFromStateBagName(bagName)
 
     if not source or source == 0 or source ~= playerServerId then return end
 
-    if not value then currentInstance = value return end
-
-    runInstanceThread()
+    currentInstance = value
 end)
